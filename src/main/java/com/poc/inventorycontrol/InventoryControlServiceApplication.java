fd3fed28da1266ba9b9f9f41989ec9a233715f3a
Rafael Jhonatan <rafael.rodrigues@bancointer.com.br>
2020-05-18 09:25:05 -0300

    Initial commit

---
+++ src/main/java/com/poc/inventorycontrol/InventoryControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.poc.inventorycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryControlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryControlServiceApplication.class, args);
+    }
+
+}
